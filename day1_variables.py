3b30ce23015bfa38f7e510e1a3f1f31d90e72eb3
Shanh17 <shanhaynes3@icloud.com>
2026-02-07 12:55:01 +0000

    Day 1: variables, input/output

--- day1_variables.py
+++ day1_variables.py
@@ -1,2 +1,18 @@
-print("My code is now on GitHub!")
+name = "Shan"
+age = "18"
+height = 1.78
 
+print(name, age, height)
+
+user_name = input("Enter your name: ")
+print("Hello", user_name)
+
+print(int(age) + 1)
+
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
